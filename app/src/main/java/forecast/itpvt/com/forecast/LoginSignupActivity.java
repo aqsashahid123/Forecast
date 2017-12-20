7c34f8ffd951dae23ed3c2553475b1a1daaf669d
Aqsa <aqsa65020@gmail>
2017-12-20 04:46:08 -0800

    AQSA

---
+++ app/src/main/java/forecast/itpvt/com/forecast/LoginSignupActivity.java
@@ -0,0 +1,19 @@
+package forecast.itpvt.com.forecast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginSignupActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_signup);
+
+
+
+    }
+}
